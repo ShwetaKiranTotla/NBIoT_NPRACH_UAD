e54323d4d7655db26398030e914cd32fc4812503
ShwetaKiranTotla <109335487+ShwetaKiranTotla@users.noreply.github.com>
2023-03-27 14:33:44 +0530

    Create activations.c
    All the required activation functions for the CNN.

---
+++ activations.c
@@ -0,0 +1,25 @@
+// All the required activation functions for the CNN
+
+#include "dl_nprach_head.h"
+
+// Function: relu()
+DATA** relu(int in_ch, int in_size, DATA **x)
+{
+    DATA **y = createMatrix_2D(in_ch, in_size);
+
+    for(int ch_i = 0; ch_i < in_ch; ch_i++)
+    {
+        for(int size_i = 0; size_i < in_size; size_i++)
+        {
+            if(x[ch_i][size_i] < 0)
+                y[ch_i][size_i] = 0;
+            else
+                y[ch_i][size_i] = x[ch_i][size_i];
+        }
+    }
+
+    // To free the memory for the output matrix of previous function
+    memory_free(x, in_ch);
+
+    return y;
+}
