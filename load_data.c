3eb9a240db6622a663951623b27f4c4e89cd4c89
ShwetaKiranTotla <109335487+ShwetaKiranTotla@users.noreply.github.com>
2023-04-05 10:30:02 +0530

    Update load_data.c

--- load_data.c
+++ load_data.c
@@ -50,35 +50,42 @@ void load_rx_mat3d(int ch, int row, int col, DATA ***rx_mat, DATA *data_in)
 		{
 			for(int k = 0; k < row; k++)
 			{
-				rx_mat[i][k][j] = data_in[c]; //changed
+				rx_mat[k][j][i] = data_in[c]; //changed  i,jk
 				c++;
 			}
 		}
 	}
 }
 
-void load_data_all(weights_t *nn_w, LUT_t *sig_lut, LUT_t *lgsm_lut)
+void load_data_all(weights_t *nn_w, LUT_t *sig_lut)//, LUT_t *lgsm_lut)
 {
 	DATA data_in[500000];
 
 
-	// LAYER 1
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L1_0_w.txt", 32*2*5, data_in);
-	load_mat3d(32, 2, 5, &(nn_w->L1_0_w), data_in);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L1_0_b.txt", 32, nn_w->L1_0_b);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L1_1_w.txt", 32, nn_w->L1_1_w);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L1_1_b.txt", 32, nn_w->L1_1_b);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L1_1_mean.txt", 32, nn_w->L1_1_mean);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L1_1_var.txt", 32, nn_w->L1_1_var);
+	// // RX DATA MATRIX
+	// read_file("RxData/rxDataMat_Err_UAD.txt", NUM_ITEM*2*SMPL_PER_PRMB, data_in);
+	// load_rx_mat3d(NUM_ITEM, 2, SMPL_PER_PRMB, rx_data->rxDataMat, data_in);
 
-	// LAYER 2
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L2_0_w.txt", 48*32*2, data_in);
+	// LAYER 1
+	read_file("/home/linux/EDL_GCC/Weights/L1_0_w.txt", 32*2*5, data_in);
+	load_mat3d(32, 2, 5, &(nn_w->L1_0_w), data_in);
+	read_file("/home/linux/EDL_GCC/Weights/L1_0_b.txt", 32, nn_w->L1_0_b);
+	read_file("/home/linux/EDL_GCC/Weights/L1_1_w.txt", 32, nn_w->L1_1_w);
+	read_file("/home/linux/EDL_GCC/Weights/L1_1_b.txt", 32, nn_w->L1_1_b);
+	read_file("/home/linux/EDL_GCC/Weights/L1_1_mean.txt", 32, nn_w->L1_1_mean);
+	read_file("/home/linux/EDL_GCC/Weights/L1_1_var.txt", 32, nn_w->L1_1_var);
+/*
+    // LAYER 2
+	read_file("/home/edc/EDL_2023_200030053/ubuntu/Weights/L2_0_w.txt", 48*32*2, data_in);
 	load_mat3d(48, 32, 2, &(nn_w->L2_0_w), data_in);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L2_0_b.txt", 48, nn_w->L2_0_b);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L2_1_w.txt", 48, nn_w->L2_1_w);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L2_1_b.txt", 48, nn_w->L2_1_b);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L2_1_mean.txt", 48, nn_w->L2_1_mean);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L2_1_var.txt", 48, nn_w->L2_1_var);
+	read_file("/home/edc/EDL_2023_200030053/ubuntu/Weights/L2_0_b.txt", 48, nn_w->L2_0_b);
+	read_file("/home/edc/EDL_2023_200030053/ubuntu/Weights/L2_1_w.txt", 48, nn_w->L2_1_w);
+	read_file("/home/edc/EDL_2023_200030053/ubuntu/Weights/L2_1_b.txt", 48, nn_w->L2_1_b);
+	read_file("/home/edc/EDL_2023_200030053/ubuntu/Weights/L2_1_mean.txt", 48, nn_w->L2_1_mean);
+	read_file("/home/edc/EDL_2023_200030053/ubuntu/Weights/L2_1_var.txt", 48, nn_w->L2_1_var);
+
+
+
 
 	// LAYER 3
 	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L3_0_w.txt", 48*48*2, data_in);
@@ -98,19 +105,30 @@ void load_data_all(weights_t *nn_w, LUT_t *sig_lut, LUT_t *lgsm_lut)
 	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L4_1_mean.txt", 192, nn_w->L4_1_mean);
 	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L4_1_var.txt", 192, nn_w->L4_1_var);
 
-	// Layer 7
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L6_0_w.txt", 1*192*1, data_in);
-	load_mat3d(1, 192, 1, &(nn_w->L6_0_w), data_in);
-	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L6_0_b.txt", 1, nn_w->L6_0_b);
+    // Layer 7
+	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L7_0_w.txt", 1*192*1, data_in);
+	load_mat3d(1, 192, 1, &(nn_w->L7_0_w), data_in);
+	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/Weights/L7_0_b.txt", 1, nn_w->L7_0_b);
+
+
+
+
+
+	// Sigmoid LUT
+	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/LUT/sigmoid_LUT_x.txt", LUT_LEN, sig_lut->x);
+	read_file("/home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/LUT/sigmoid_LUT_y.txt", LUT_LEN, sig_lut->y);
+
+	// Logsoftmax LUT
+	*/
 }
 
-void load_rx_data_ant1(rxData_t *rx_data)
+void load_rx_data_ant2(rxData_ant2_t *rx_data)
 {
 	DATA data_in[500000];
-
+	//DATA data_in[NUM_INP_SMPL];
+    //printf("%d\n",35);
 	// RX DATA MATRIX
-	read_file("home/edc/EDL_2023_200030053/DL_Receiver_C_Fixed_Rxsignal/CodeBlocks_Project/RxData/rxDataMat_Err_UAD.txt", NUM_ITEM*2*SMPL_PER_PRMB, data_in);
+    read_file("/home/linux/EDL_GCC/RxData/rxDataMat_Err_UAD.txt", NUM_ITEM*2*SMPL_PER_PRMB, data_in);
 	//read_file("RxData/rxDataMat_Err_UAD.txt", NUM_ITEM*2*SMPL_PER_PRMB, data_in);
-	load_rx_mat3d(NUM_ITEM, 2, SMPL_PER_PRMB, rx_data->rxDataMat, data_in);
+        //load_rx_mat3d(NUM_ITEM, 2, SMPL_PER_PRMB, rx_data->rxDataMat, data_in);
 }
-
