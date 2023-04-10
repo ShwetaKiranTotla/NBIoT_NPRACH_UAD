37fe2978d45b588d9a711b58a9400c85b9f48768
ShwetaKiranTotla <109335487+ShwetaKiranTotla@users.noreply.github.com>
2023-04-10 10:17:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -25,7 +25,22 @@ void print_mat1d(int size, DATA *mat)
 
 int main()
 {
-	if(NUM_RX_ANTENNA == 2)
+//printf("%d\n",31);
+/*
+	if(NUM_RX_ANTENNA == 1)
+	{
+		// Structure to store all weights and biases of CNN
+		weights_t nn_w;
+		rxData_t rx_data;
+		LUT_t sig_lut;
+		LUT_t lgsm_lut;
+		rx_data.rxDataMat = createMatrix_3D(NUM_ITEM, 2 , SMPL_PER_PRMB);
+		load_data_all(&nn_w, &sig_lut, &lgsm_lut);
+		load_rx_data_ant1(&rx_data);
+		UAD_PRM0_CVA12_eval(&nn_w, &rx_data, &sig_lut, &lgsm_lut);
+	}
+*/
+	if(NUM_RX_ANTENNA == 1)
 	{
 		// Structure to store all weights and biases of CNN
 		weights_t nn_w;
@@ -33,7 +48,10 @@ int main()
 		LUT_t sig_lut;
 
 		rx_data.rxDataMat = createMatrix_3D(NUM_ITEM, 2 , SMPL_PER_PRMB);//changed
+		//rx_data.rxDataMat_2 = createMatrix_3D(NUM_ITEM, 2 , SMPL_PER_PRMB);
+
 		load_data_all(&nn_w, &sig_lut);//, &lgsm_lut);
+//		printf("%d\n",3);
 		load_rx_data_ant2(&rx_data);
 		UAD_PRM0_CVA12_eval_ant2(&nn_w, &rx_data, &sig_lut);//, &lgsm_lut);
 	}
@@ -43,4 +61,5 @@ int main()
 	}
 
 	//print_mat3d(NUM_ITEM, 2, 160, &rx_data.rxDataMat);
+	//print_mat1d(LUT_LEN, sig_lut.y);
 }
